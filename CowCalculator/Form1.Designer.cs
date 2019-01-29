8516d1c48d330199b1258cc6ff7235235c9fdc28
Tavia Sterling <TaviaSterling@outlook.com>
2019-01-29 12:18:39 -0700

    final layout

--- CowCalculator/Form1.Designer.cs
+++ CowCalculator/Form1.Designer.cs
@@ -30,12 +30,13 @@
         {
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.calculate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(127, 75);
+            this.numericUpDown1.Location = new System.Drawing.Point(141, 75);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
             this.numericUpDown1.TabIndex = 0;
@@ -43,7 +44,7 @@
             // 
             // calculate
             // 
-            this.calculate.Location = new System.Drawing.Point(127, 174);
+            this.calculate.Location = new System.Drawing.Point(78, 154);
             this.calculate.Name = "calculate";
             this.calculate.Size = new System.Drawing.Size(102, 23);
             this.calculate.TabIndex = 1;
@@ -51,17 +52,29 @@
             this.calculate.UseVisualStyleBackColor = true;
             this.calculate.Click += new System.EventHandler(this.calculate_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(12, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 15);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Number of Cows";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.calculate);
             this.Controls.Add(this.numericUpDown1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Cow Calculator";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +82,7 @@
 
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
